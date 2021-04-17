119679f68f019c0491b77ec1ac2ea0fc5ecd5fd8
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-04-17 13:26:00 -0300

    testing array types

---
+++ typescriptFeaturesTest/array.ts
@@ -0,0 +1,34 @@
+const carMakers = ["red", "blue"];
+const dates = [new Date(), new Date()];
+
+const carsByMake: string[][][] = [
+  [
+    ["test", "test"],
+    ["test", "test"],
+  ],
+  [
+    ["test", "test"],
+    ["test", "test"],
+  ],
+  [
+    ["test", "test"],
+    ["test", "test"],
+  ],
+];
+
+// Help with inference when extracting values
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+
+// carMakers.push(2);
+
+carMakers.map((car: string): string => {
+  return car;
+});
+
+// flexible types
+
+const importantDates: (Date | string)[] = [new Date(), "string"];
