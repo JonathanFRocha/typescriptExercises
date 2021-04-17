dd2012a074101364ab2513688fd293a9204fa3d5
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-04-17 10:01:59 -0300

    testing objects and functions

--- typescriptFeaturesTest/functions.ts
+++ typescriptFeaturesTest/functions.ts
@@ -9,3 +9,38 @@ const subtract = (a: number, b: number) => {
 function divide(a: number, b: number): number {
   return a / b;
 }
+
+const multiply = (a: number, b: number): number => {
+  return a + b;
+};
+
+const logger = (text: string): void => {
+  console.log(text);
+};
+
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+const forecast = {
+  date: new Date(),
+  weather: "sunny",
+};
+
+const logWeather = (forecast: { date: Date; weather: string }): void => {
+  console.log(forecast.weather);
+};
+
+logWeather(forecast);
+
+const logWeather2 = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+};
+
+logWeather2(forecast);
