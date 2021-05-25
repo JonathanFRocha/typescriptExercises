bbda850169f32125135c8ae6743d619b290b8e64
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-05-25 10:40:21 -0300

    tested decorators

--- typescriptFeaturesTest/decorators.ts
+++ typescriptFeaturesTest/decorators.ts
@@ -5,13 +5,23 @@ class Boat {
     return `this boats color is ${this.color}`;
   }
 
-  @testDecorator
+  @logError
   pilot(): void {
+    throw new Error();
     console.log("swish");
   }
 }
 
-function testDecorator(target: any, key: string): void {
-  console.log("target:", target);
-  console.log("key:", key);
+function logError(target: any, key: string, desc: PropertyDescriptor): void {
+  const method = desc.value;
+
+  desc.value = function () {
+    try {
+      method();
+    } catch (e) {
+      console.log("Opps boat sunk");
+    }
+  };
 }
+
+new Boat().pilot();
